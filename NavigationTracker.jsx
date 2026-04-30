c0b2fe17ecaa537d0c7c00dd67e228aaac9f9aa4
Shen212 <nathanshen47@gmail.com>
2026-04-30 17:45:48 -0400

    Revert "Add files via upload"
    This reverts commit bf15c5fb81704d42dab01c0345666a174a76baf0.

--- NavigationTracker.jsx
+++
@@ -1,5 +0,0 @@
-// NavigationTracker previously logged page views to Base44.
-// Base44 has been removed, so this is now a no-op component.
-export default function NavigationTracker() {
-  return null;
-}
